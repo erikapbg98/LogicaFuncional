97de72e8d805fd1ca4a701ee70c56c75cab862aa
erikapbg98 <eripalomita97@gmail.com>
2020-11-06 12:51:15 -0700

    Primeras Funciones

---
+++ funcional/introduccion/funciones.hs
@@ -0,0 +1,14 @@
+--Como hacer una suma
+--public void suma(a,b)(return a+b;)
+--ghc -o ejecutable ./funciones.hs
+--ghci entrar al programa prelude
+
+suma a b = a + b
+resta a b = a - b
+sumar a b c d e = a + b + c + d + e
+division a b = a `div`  b
+pregunta a = not a 
+{- resta a b = a - b
+sumar a b c d e = a + b + c + d + e
+division a b = a `div` b
+pregunta a = not a -}
