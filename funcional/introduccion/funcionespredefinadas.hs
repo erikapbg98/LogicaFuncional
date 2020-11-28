f560f4fad5077b5e1f2eda14ea811481a881a7ce
erikapbg98 <eripalomita97@gmail.com>
2020-11-27 22:31:20 -0700

    funcionespredefinida

---
+++ funcional/introduccion/funcionespredefinadas.hs
@@ -0,0 +1,27 @@
+funcion1 x l = abs(x+l);
+funcion2 b = ceiling (negate(tan(cos(max 50 b))));
+funcion3 x j = succ (x+j);
+funcion4 x y n m= min (x+y) (n+m);
+funcion5 x y k l= max (x+y) (k+l);
+funcion6 n j = div n j;
+funcion7 z x v b n m = mod (z*x+v) (b+n*m);
+funcion8 x v = even (x*v);
+funcion9 a s d = floor (a+s*d);
+funcion10 x y b n = gcd (x+y) (b+n);
+funcion11  v = not v;
+funcion12 x = odd x;
+funcion13 x l = succ(x*l);
+funcion14 s v = signum (s*v);
+funcion15 c v b n m = snd((c*v),(b+n*m));
+
+
+
+
+
+
+
+
+
+
+
+
